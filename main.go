44737e205a17c5f123f1006b6fd87337db83287c
Jim Huxling <jim.huxling@gmail.com>
2018-03-03 19:37:31 +0100

    clean main function

--- main.go
+++ main.go
@@ -10,12 +10,11 @@ import (
 var VERSION = "0.0.0-src" //set with ldflags
 
 func main() {
+
 	s := server.Server{
 		Title:      "Cloud Torrent",
 		Port:       3000,
 		ConfigPath: "cloud-torrent.json",
-		
-		//HostName:   "example.org", // domain name on which https is enabled
 	}
 
 	o := opts.New(&s)
